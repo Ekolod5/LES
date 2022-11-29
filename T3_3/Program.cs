1ba1e30cfbc74fd5e34a2ee79ea53b2796ba4792
lena <ekolod5@mail.ru>
2022-11-29 15:55:57 +0300

    Функции, вид 2

---
+++ T3_3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Вид 2_1
+
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+Method2(msg: "Текст сообщения: ");
+
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+Method21("Текст", 4);   // Сколько раз (4) хотим увидеть это сообщение
+
