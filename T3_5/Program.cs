fb8acb43a934acfc1a7fcd4487b6f9181bc36806
lena <ekolod5@mail.ru>
2022-11-29 16:14:13 +0300

    Функции, вид 4

---
+++ T3_5/Program.cs
@@ -0,0 +1,16 @@
+﻿// Вид 4
+
+string Method4(int count, string text)   // string строка
+{
+    int i = 0;
+    string result = string.Empty; // пустая строка
+    
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+string res = Method4(10, "z");
+Console.WriteLine(res);
